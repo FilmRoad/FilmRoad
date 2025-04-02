8e79a0465b93eeddc9aafb721681ce15abd98071
juj990717 <juj990717@gmail.com>
2025-04-02 17:46:31 +0900

    edited

--- FilmLoad/ViewController.swift
+++ FilmLoad/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //dk
+        //d
         // Do any additional setup after loading the view.
     }
 
