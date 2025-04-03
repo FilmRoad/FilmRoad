9bd763fdce9816c9d98168bfca7274d480aa1301
ksiomng <nadana0929@gmail.com>
2025-04-03 11:31:00 +0900

    Feat: api 키 숨기기

---
+++ FlimRoad/Key.swift
@@ -0,0 +1,13 @@
+//
+//  Key.swift
+//  FlimRoad
+//
+//  Created by Song Kim on 4/3/25.
+//
+
+import Foundation
+
+class Key {
+    static let mediaKey = Bundle.main.infoDictionary?["mediaKey"] as! String
+    static let filmRoadKey = Bundle.main.infoDictionary?["filmRoadKey"] as! String
+}
