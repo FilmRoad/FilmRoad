1ad47ae4016ca00ff999c2b24535b6e6dffddbb7
ksiomng <nadana0929@gmail.com>
2025-04-04 14:47:37 +0900

    Fix: 유진님 코드도 네비게이션 연결해주기

--- FilmRoad/SearchTableViewController.swift
+++ FilmRoad/SearchTableViewController.swift
@@ -26,33 +26,39 @@ class SearchTableViewController: UITableViewController {
             self.tableView.reloadData()
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return titleArr.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath)
-
+        
         let title = titleArr[indexPath.row]
         let lblText = cell.viewWithTag(1) as? UILabel
         
         lblText?.text = title.mediaTitle
         
-
+        
         return cell
     }
     
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as? ListTableViewController
+        guard let indexPath = tableView.indexPathForSelectedRow else {return}
+        let selectedItem = titleArr[indexPath.row]
+        destinationVC?.titleName = selectedItem.mediaTitle
+    }
 }
+
 extension SearchTableViewController: UISearchBarDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchWithKeyword(searchBar.text)
         searchBar.resignFirstResponder()
     }
-
+    
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         print("취소")
     }
